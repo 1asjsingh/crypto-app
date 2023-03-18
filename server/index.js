6bbce9693925b93c7e8da5764ec7f06c91564a04
Amaninder Jhinger <1asjsingh@gmail.com>
2023-03-18 11:45:03 +0000

    Implemented API controller with cache

--- server/index.js
+++ server/index.js
@@ -1,36 +1,26 @@
 const express = require("express");
-require("dotenv").config();
 const app = express();
+const port = 4000;
+
+require("dotenv").config();
 const cors = require("cors");
 const bodyParser = require("body-parser");
-//const admin = require("firebase-admin");
-const db = require("./firestore");
-const port = 4000;
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-const axios = require("axios");
-
-/*const serviceAccount = require(process.env.GOOGLE_APPLICATION_CREDENTIALS);
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://crypto-project-21534.firebaseio.com",
-});
-
-const db = admin.firestore();*/
-
 const transactionRoutes = require("./routes/transactionRoutes");
 const portfolioRoutes = require("./routes/portfolioRoutes");
 const leaderboardRoutes = require("./routes/leaderboardRoutes");
 const userRoutes = require("./routes/userRoutes");
+const apiRoutes = require("./routes/apiRoutes");
 
 app.use("/transaction", transactionRoutes);
 app.use("/portfolio", portfolioRoutes);
 app.use("/leaderboard", leaderboardRoutes);
 app.use("/user", userRoutes);
+app.use("/api", apiRoutes);
 
 app.listen(port, () => {
   console.log(`Express server listening on PORT ${port}`);
