de7f584968652daadb787abc8caafdced028bac1
Amaninder Jhinger <1asjsingh@gmail.com>
2023-03-19 05:48:07 +0000

    Added Error HTML Jade view

--- express-server/app.js
+++ express-server/app.js
@@ -6,7 +6,8 @@ const morgan = require("morgan");
 // const cors = require('cors');
 
 // Load `.env` variables into `process.env`
-require("dotenv").config();
+// FIXME: const node_env = process.env.NODE_ENV ? "." + process.env.NODE_ENV : "";
+require("dotenv").config(); // FIXME: Use - .config(`.env${node_env}`) - ?
 
 const transactionRoutes = require("./routes/transactionRoutes");
 const portfolioRoutes = require("./routes/portfolioRoutes");
@@ -16,6 +17,11 @@ const apiRoutes = require("./routes/apiRoutes");
 
 const app = express();
 
+// Jade view engine setup (for rendering errors)
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
+app.use(express.static(path.join(__dirname, "public")));
+
 // Middleware registration (functions that should access all request objects)
 app.use(morgan("dev"));
 app.use(express.json());
@@ -24,16 +30,16 @@ app.use(express.urlencoded({ extended: false }));
 // app.use(cors());
 
 // Sanity check
-app.use('/', (req, res) => {
-  return res.send('<p>Hello, <code>crypto-app</code>!</p>');
+app.all("/hello", (req, res) => {
+  return res.send("<p>Hello, <code>crypto-app</code>!</p>");
 });
 
 // crypto-app API route handler registration
-app.use('/transaction', transactionRoutes);
-app.use('/portfolio', portfolioRoutes);
-app.use('/leaderboard', leaderboardRoutes);
-app.use('/user', userRoutes);
-app.use('/api', apiRoutes);
+app.use("/transaction", transactionRoutes);
+app.use("/portfolio", portfolioRoutes);
+app.use("/leaderboard", leaderboardRoutes);
+app.use("/user", userRoutes);
+app.use("/api", apiRoutes);
 
 // Catch requests that haven't been handled by the route handlers and forward
 // create a 404 error, and forward to error handler
@@ -45,8 +51,11 @@ app.use(function (req, res, next) {
 app.use(function (err, req, res, next) {
   // Only provide error during development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+  // NOTE: If you don't render error, change status() -> sendStatus() to
+  //       terminate request-response cycle.
   res.status(err.status || 500);
+  res.render("error", { title: "Error" });
 });
 
 module.exports = app;
