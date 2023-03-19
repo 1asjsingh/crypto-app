e0312fa37e9b2184ac9ba66dabb2dd3f9d440b63
Amaninder Jhinger <1asjsingh@gmail.com>
2023-03-19 22:35:01 +0000

    Rename axios.js to coingecko.js and move to axios pkg for commonly used axios instances in React client

--- react-client/src/axios/coingecko.js
+++ react-client/src/axios/coingecko.js
@@ -1,8 +1,6 @@
 import axios from "axios";
 
-const instance = axios.create({
+export default axios.create({
   baseURL: "https://api.coingecko.com/api/v3/",
   headers: { accept: "application/json" },
 });
-
-export default instance;
